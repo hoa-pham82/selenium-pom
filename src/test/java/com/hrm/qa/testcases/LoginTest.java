0d84a2258c326423e2c5acd7478db11212dce43c
Hoa Pham <phamtthanhhoa@gmail.com>
2023-09-08 00:15:45 +0700

    update login test

--- src/test/java/com/hrm/qa/testcases/LoginTest.java
+++ src/test/java/com/hrm/qa/testcases/LoginTest.java
@@ -33,6 +33,9 @@ public class LoginTest extends BaseSetup {
   @Test
   public void verifyLoginSuccessfully() {
     dashboardPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    boolean dashBoardHeader = dashboardPage.verifyDashBoardHeader();
+    Assert.assertTrue(dashBoardHeader);
+
   }
 
   @AfterClass
