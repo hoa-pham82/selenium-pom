6f2990aac748cbdeb4accb1750614456abdecc33
Hoa Pham <phamtthanhhoa@gmail.com>
2023-09-07 22:01:32 +0700

    Add dashboard page

--- src/main/java/com/hrm/qa/pages/DashboardPage.java
+++ src/main/java/com/hrm/qa/pages/DashboardPage.java
@@ -1,7 +1,29 @@
 package com.hrm.qa.pages;
 
 import com.hrm.qa.base.BaseSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class DashboardPage extends BaseSetup {
+
+  @FindBy(xpath = "//h6[text()='Dashboard']")
+  WebElement dashBoard;
+
+  @FindBy(xpath = "//a")
+  WebElement title;
+
+  public DashboardPage() {
+    PageFactory.initElements(driver, this);
+  }
+
+  public boolean navigateToDashBoard() {
+    return this.dashBoard.isDisplayed();
+  }
+
+  public void navigateToPage(String title) {
+    this.title.findElement(By.xpath(String.format("//*[text()='%s']", title)));
+  }
 
 }
