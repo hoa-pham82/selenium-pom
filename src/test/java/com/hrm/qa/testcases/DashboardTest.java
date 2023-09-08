b34727d8780ec9ecf90ab0d4969ac7cab5b8f288
Hoa Pham <phamtthanhhoa@gmail.com>
2023-09-09 01:18:00 +0700

    update DashboardTest

--- src/test/java/com/hrm/qa/testcases/DashboardTest.java
+++ src/test/java/com/hrm/qa/testcases/DashboardTest.java
@@ -6,6 +6,7 @@ import com.hrm.qa.pages.DashboardPage;
 import com.hrm.qa.pages.LoginPage;
 import com.hrm.qa.pages.RecruitmentPage;
 import com.hrm.qa.util.TestUtil.PageTitle;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -32,11 +33,15 @@ public class DashboardTest extends BaseSetup {
   @Test
   public void verifyToNavigateToAdminPage() {
     adminPage = dashboardPage.navigateToPage(PageTitle.ADMIN.value);
+    boolean isDisplayed = adminPage.verifyAdminHeaderIsDisplayed();
+    Assert.assertTrue(isDisplayed, "The admin page header is not displayed!");
   }
 
   @Test
   public void verifyToNavigateToRecruitmentPage() {
     recruitmentPage = dashboardPage.navigateToPage(PageTitle.RECRUITMENT.value);
+    boolean isDisplayed = recruitmentPage.verifyRecruitmentHeaderIsDisplayed();
+    Assert.assertTrue(isDisplayed, "The recruitment page header is not displayed");
   }
 
 
