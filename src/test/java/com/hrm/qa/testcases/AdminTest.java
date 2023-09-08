df377b0bdca1211061497b09e1ead749c9321d6d
Hoa Pham <phamtthanhhoa@gmail.com>
2023-09-09 02:58:49 +0700

    add admin test

--- src/test/java/com/hrm/qa/testcases/AdminTest.java
+++ src/test/java/com/hrm/qa/testcases/AdminTest.java
@@ -1,6 +1,7 @@
 package com.hrm.qa.testcases;
 
 import com.hrm.qa.base.BaseSetup;
+import com.hrm.qa.pages.AdminPage;
 import com.hrm.qa.pages.DashboardPage;
 import com.hrm.qa.pages.LoginPage;
 import com.hrm.qa.util.TestUtil.PageTitle;
@@ -9,12 +10,12 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class DashboardTest extends BaseSetup {
-
+public class AdminTest extends BaseSetup {
 
   DashboardPage dashboardPage;
+  AdminPage adminPage;
 
-  public DashboardTest() {
+  public AdminTest() {
     super();
   }
 
@@ -23,22 +24,14 @@ public class DashboardTest extends BaseSetup {
     initialization();
     LoginPage loginPage = new LoginPage();
     dashboardPage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    adminPage = dashboardPage.navigateToPage(PageTitle.ADMIN.value);
   }
 
-  @Test
-  public void verifyToNavigateToAdminPage() {
-    dashboardPage.navigateToPage(PageTitle.ADMIN.value);
-    boolean isDisplayed = verifyPageHeaderIsDisplayed(PageTitle.ADMIN.value);
-    Assert.assertTrue(isDisplayed, "The admin page header is not displayed!");
+  @Test(enabled = false)
+  public void verifyAdminPageHeader() {
+    Assert.assertTrue(verifyPageHeaderIsDisplayed(PageTitle.ADMIN.value),
+        "The admin page header is not displayed!");
   }
-
-  @Test
-  public void verifyToNavigateToRecruitmentPage() {
-    dashboardPage.navigateToPage(PageTitle.RECRUITMENT.value);
-    boolean isDisplayed = verifyPageHeaderIsDisplayed(PageTitle.RECRUITMENT.value);
-    Assert.assertTrue(isDisplayed, "The recruitment page header is not displayed");
-  }
-
 
   @AfterClass
   public void teardown() {
